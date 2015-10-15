707cc43550bbbd5cd3ee789cfcc93854c2190c6c
Kpwbo <jhoayek@gmail.com>
2015-10-15 15:24:17 -0400

    RSpec will now check the 1-minute constraint

--- spec/euler_spec.rb
+++ spec/euler_spec.rb
@@ -4,8 +4,14 @@ answers = [233168, 4613732, 6857, 906609]
 
 describe Euler do
   (1..4).each do |problem|
+    time = Time.now
+    answer = Euler.run(problem)
+    time_taken = Time.now - time
     it "gives the correct answer for Problem #{problem}" do
-      expect(Euler.run(problem)).to eq(answers[problem-1]) 
+      expect(answer).to eq(answers[problem-1]) 
+    end
+    it "takes less than 1 minute to give the answer of Problem #{problem}" do
+      expect(time_taken).to be < 60
     end
   end
 end
